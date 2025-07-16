7dbdae94da59bd02b4517e27fafb18f35763daf9
pankajmehta07 <123pankajmehta@gmail.com>
2025-07-16 14:22:53 +0545

    Added signup form modal

--- Auction/static/Auction/js/app.js
+++ Auction/static/Auction/js/app.js
@@ -1,7 +1,7 @@
 
-function togglePasswordVisibility() {
-    const passwordInput = document.getElementById("inputPassword");
-    const icon = document.getElementById("togglePasswordIcon");
+function togglePasswordVisibility(inputId, iconId) {
+    const passwordInput = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
     
     if (passwordInput.type === "password") {
     passwordInput.type = "text";
@@ -13,3 +13,39 @@ function togglePasswordVisibility() {
     icon.classList.add("fa-eye-slash");
     }
 }
+
+function handleFocus(element,divID){
+    console.log(divID);
+    const passDiv = document.getElementById(divID);
+    passDiv.classList.add('highlight-border');
+}
+function handleBlur(element,divID){
+    console.log(divID);
+    const passDiv = document.getElementById(divID);
+    passDiv.classList.remove('highlight-border');
+}
+
+
+function switchType(type) {
+    if (type === 'bidder') {        
+      document.getElementById('usernameInputLabel').innerText = "Citizenship No";
+      document.getElementById('userType').value = "Bidder";
+      document.getElementById('usernameInput').placeholder = "Enter citizenship number (username)";
+      document.getElementById('nameInput').placeholder = "Enter full name";
+      document.getElementById('usernameInput').value = "";
+      document.getElementById('nameInput').value = "";
+      document.getElementById('contactInput').value = "";
+      document.getElementById('addressInput').value = "";
+      document.getElementById('registerPassword').value = "";
+    } else {
+        document.getElementById('usernameInputLabel').innerText = "Registration No";
+      document.getElementById('userType').value = "Organization";
+      document.getElementById('usernameInput').value = "";
+      document.getElementById('nameInput').value = "";
+      document.getElementById('contactInput').value = "";
+      document.getElementById('addressInput').value = "";
+      document.getElementById('registerPassword').value = "";
+        document.getElementById('usernameInput').placeholder = "Enter registration number (username)";
+        document.getElementById('nameInput').placeholder = "Enter oraganization name";
+    }
+  }
