0cfbbbe17b219dab5639fe911992e574a863d23e
Nischal-D <nischal.079bei016@tcioe.edu.np>
2025-07-29 21:30:19 +0545

    Added profile section and edit profile page

--- Auction/urls.py
+++ Auction/urls.py
@@ -16,5 +16,7 @@ urlpatterns = [
     path("item/<int:itemID>",views.item, name="item"),
     path("items",views.showItems, name="showItems"),
     path("edit",views.editItem, name="edit"),
-    path("profile",views.profile, name="profile")
+    path("profile",views.profile, name="profile"),
+    path("editProfile",views.editProfile, name="editProfile")
+
 ]
