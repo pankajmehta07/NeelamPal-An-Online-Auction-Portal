3919a878d03fba141d1c86be3876cd2fa5254dd2
pankajmehta07 <123pankajmehta@gmail.com>
2025-08-07 20:18:32 +0545

    trying to show media by changing debug to true

--- NeelamPal/settings.py
+++ NeelamPal/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0(%hs+*=3ky%^3^t9lda%my(lu+zgv-ea*cczdumg#h7&u)+16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '') == 'True'
+# DEBUG = os.environ.get('DEBUG', '') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
